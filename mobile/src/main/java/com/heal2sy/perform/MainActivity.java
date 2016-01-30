5bd0beb6f80e1c5ce66ad23acb07e56f6f72c3ba
StephaneC <Stephane.castrec@gmail.com>
2016-01-30 17:36:46 +0100

    swiper for tutorial

--- mobile/src/main/java/com/heal2sy/perform/MainActivity.java
+++ mobile/src/main/java/com/heal2sy/perform/MainActivity.java
@@ -1,42 +1,64 @@
 package com.heal2sy.perform;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.heal2sy.perform.fragment.TutoFragment;
+
+public class MainActivity extends FragmentActivity {
+
+    ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.explain).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Fonctionnalité en cours de dev", Toast.LENGTH_SHORT).show();
-            }
-        });
-        findViewById(R.id.relax).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Fonctionnalité en cours de dev", Toast.LENGTH_SHORT).show();
-            }
-        });
-        findViewById(R.id.excercies).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Fonctionnalité en cours de dev", Toast.LENGTH_SHORT).show();
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new PagerAdapter(this.getSupportFragmentManager()));
 
-            }
-        });
-        findViewById(R.id.metro).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Fonctionnalité en cours de dev", Toast.LENGTH_SHORT).show();
-
+                startActivity(new Intent(MainActivity.this, MenuActivity.class));
             }
         });
+
+    }
+
+    public class PagerAdapter extends FragmentPagerAdapter {
+
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment f = null;
+            switch(position){
+                case 0:
+                    f =  TutoFragment.newInstance(R.string.text_tuto1, android.R.drawable.ic_menu_help);
+                    break;
+                case 1:
+                    f =  TutoFragment.newInstance(R.string.text_tuto2, android.R.drawable.ic_delete);
+                    break;
+                case 2:
+                    f =  TutoFragment.newInstance(R.string.text_tuto3, android.R.drawable.ic_dialog_email);
+                    break;
+            }
+            return f;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
     }
 }
